b7502cc6524520de40697171c2cd37198216e437
Evgeniq Nikonov <zhenya.nikonov1999@gmail.com>
2021-12-14 19:38:26 +0300

    HeaderMenuDone

--- js/mainHeaderMenu.js
+++ js/mainHeaderMenu.js
@@ -4,20 +4,27 @@
 function init() {
 	let pictureForChange = document.getElementById('barImg');
 	pictureForChange.onclick = imgChanger;
+	window.onresize = sizeshecker;
 }
 
 function imgChanger(){
 	let pictureForChange = document.getElementById('mainNavMen');
 	if(pictureForChange.getAttribute("class") === "mainNavigationMenu"){
-		console.log("CAS")
-		pictureForChange.setAttribute("class", "crossImages");
+		pictureForChange.setAttribute("class", "mainNavMenu");
 	}
 	else {
 		pictureForChange.setAttribute("class", "mainNavigationMenu");
 	}
 }
 
-
+function sizeshecker(){
+	let pictureForChange = document.getElementById("mainNavMen");
+	let fa_barsMenu = document.getElementById("fa-barsMenu");
+	if(window.screen.width >= 767){
+		pictureForChange.setAttribute("class", "mainNavigationMenu");
+		fa_barsMenu.checked = false;
+	} 
+}
 
 
 window.onload = init;
